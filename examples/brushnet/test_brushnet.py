c5d2e38c18ce206aab00e2b5a30fc7c816574222
machineko <machineko@protonmail.com>
2024-07-24 13:40:40 +0200

    test

--- examples/brushnet/test_brushnet.py
+++ examples/brushnet/test_brushnet.py
@@ -3,7 +3,8 @@ import torch
 import cv2
 import numpy as np
 from PIL import Image
-
+from pathlib import Path
+import os
 # choose the base model here
 base_model_path = "data/ckpt/realisticVisionV60B1_v51VAE"
 # base_model_path = "runwayml/stable-diffusion-v1-5"
@@ -13,13 +14,55 @@ brushnet_path = "data/ckpt/segmentation_mask_brushnet_ckpt"
 
 # choose whether using blended operation
 blended = False
+captions = {
+    'Cityline_Family': "Man and women with a child walking close to the shore with city and sea in the background.",
+    'Meadow_Family': "Family on the meadow with trees and forest in the background.",
+    'NY_Night': "New york city at the night.",
+    'City_Vertical': "Asian city.",
+    "Tokyo_Guy": "City of tokyo at night.",
+    "Beach": "Beach full of people with apartaments in the background."
+}
 
-# input source image / mask image path and the text prompt
-image_path="examples/brushnet/src/test_image.jpg"
-mask_path="examples/brushnet/src/test_mask.jpg"
-caption="A cake on the table."
+def pred(img_name: str):
+    # if "Cityline_Family" in img_name or "Meadow_Family" in img_name:
+    #     return
+    base_path = "data/_Benchmark_photos/"
+    all_masks = [i for i in os.listdir(f"{base_path}/Masks") if i.__contains__(img_name[:-4])]
+    image_path = Path(base_path, img_name).__str__()
+    caption = captions[img_name[:-4]]
+    for mask in all_masks:
+        init_image = cv2.imread(image_path)[:,:,::-1]
+        mask_image = 1.*(cv2.imread(Path(base_path, "Masks", mask).__str__()).sum(-1)>255)[:,:,np.newaxis]
+        init_image = init_image * (1 - mask_image)
 
-# conditioning scale
+        brushnet_conditioning_scale=1.0
+        h,w,_ = init_image.shape
+        if w<h:
+            scale=512/w
+        else:
+            scale=512/h
+        new_h=int(h*scale)
+        new_w=int(w*scale)
+
+
+        init_image = Image.fromarray(init_image.astype(np.uint8)).convert("RGB").resize((new_w, new_h), Image.BICUBIC)
+        mask_image = Image.fromarray(mask_image.astype(np.uint8).repeat(3,-1)*255).convert("RGB").resize((new_w, new_h), Image.NEAREST)
+
+        generator = torch.Generator("cuda").manual_seed(1234)
+
+        image = pipe(
+            prompt=caption, 
+            image=init_image, 
+            mask=mask_image, 
+            num_inference_steps=50,
+            generator=generator,
+            brushnet_conditioning_scale=brushnet_conditioning_scale
+        ).images[0]
+
+        image.save(f"out/sd15_brushnet_{mask}")
+
+
+
 brushnet_conditioning_scale=1.0
 
 brushnet = BrushNetModel.from_pretrained(brushnet_path, torch_dtype=torch.float16)
@@ -34,36 +77,8 @@ pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 # memory optimization.
 pipe.enable_model_cpu_offload()
 
-init_image = cv2.imread(image_path)[:,:,::-1]
-mask_image = 1.*(cv2.imread(mask_path).sum(-1)>255)[:,:,np.newaxis]
-init_image = init_image * (1-mask_image)
 
-init_image = Image.fromarray(init_image.astype(np.uint8)).convert("RGB")
-mask_image = Image.fromarray(mask_image.astype(np.uint8).repeat(3,-1)*255).convert("RGB")
+all_files = [i for i in os.listdir("data/_Benchmark_photos") if "jpg" in i or ".png" in i]
 
-generator = torch.Generator("cuda").manual_seed(1234)
-
-image = pipe(
-    caption, 
-    init_image, 
-    mask_image, 
-    num_inference_steps=50, 
-    generator=generator,
-    brushnet_conditioning_scale=brushnet_conditioning_scale
-).images[0]
-
-if blended:
-    image_np=np.array(image)
-    init_image_np=cv2.imread(image_path)[:,:,::-1]
-    mask_np = 1.*(cv2.imread(mask_path).sum(-1)>255)[:,:,np.newaxis]
-
-    # blur, you can adjust the parameters for better performance
-    mask_blurred = cv2.GaussianBlur(mask_np*255, (21, 21), 0)/255
-    mask_blurred = mask_blurred[:,:,np.newaxis]
-    mask_np = 1-(1-mask_np) * (1-mask_blurred)
-
-    image_pasted=init_image_np * (1-mask_np) + image_np*mask_np
-    image_pasted=image_pasted.astype(image_np.dtype)
-    image=Image.fromarray(image_pasted)
-
-image.save("output.png")
+for f in all_files:
+    pred(f)
